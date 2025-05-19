d5b6f2dbf73a16c304629b3a49b9bca337dad9f4
CandleStudent <rolltake@yandex.ru>
2025-05-20 02:53:40 +0300

    fix CustomerAddressRepository: set right Id class instead of Long

--- backend/src/main/java/ru/delivery/repository/CustomerAddressRepository.java
+++ backend/src/main/java/ru/delivery/repository/CustomerAddressRepository.java
@@ -3,8 +3,9 @@ package ru.delivery.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.delivery.entity.CustomerAddress;
+import ru.delivery.entity.CustomerAddress.CustomerAddressId;
 
 @Repository
-public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Long> {
+public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, CustomerAddressId> {
 
 }
