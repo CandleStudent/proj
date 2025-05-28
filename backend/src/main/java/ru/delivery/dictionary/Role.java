0c21da78b078786ade562057c2039a68d694a10f
CandleStudent <rolltake@yandex.ru>
2025-05-28 19:35:26 +0300

    add fromValue() and toValue() for enums

--- backend/src/main/java/ru/delivery/dictionary/Role.java
+++ backend/src/main/java/ru/delivery/dictionary/Role.java
@@ -1,7 +1,20 @@
 package ru.delivery.dictionary;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Role {
 
   CUSTOMER, RESTAURANT_MASTER, COURIER;
+
+  @JsonCreator
+  public Role fromValue(String text) {
+    return Enum.valueOf(Role.class, text);
+  }
+
+  @JsonValue
+  public String toValue() {
+    return this.name();
+  }
 
 }
--- backend/src/main/java/ru/delivery/dictionary/UserStatus.java
+++ backend/src/main/java/ru/delivery/dictionary/UserStatus.java
@@ -1,7 +1,20 @@
 package ru.delivery.dictionary;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum UserStatus {
 
   ACTIVE, BLOCKED, DEACTIVATED;
+
+  @JsonCreator
+  public UserStatus fromValue(String text) {
+    return Enum.valueOf(UserStatus.class, text);
+  }
+
+  @JsonValue
+  public String toValue() {
+    return this.name();
+  }
 
 }
