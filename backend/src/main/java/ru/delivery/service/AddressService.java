cc34e9a755a4af74aa16b5dc25343ee804a5dd98
CandleStudent <rolltake@yandex.ru>
2025-05-31 01:29:12 +0300

    Добавить обращение к геосаджестеру от яндекса для валидации поступающих адресов и приведения их к единому формату.

--- backend/src/main/java/ru/delivery/service/AddressService.java
+++ backend/src/main/java/ru/delivery/service/AddressService.java
@@ -3,24 +3,52 @@ package ru.delivery.service;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.delivery.dto.AddressDto;
+import ru.delivery.exception.BusinessLogicException;
 import ru.delivery.mapper.AddressMapper;
 import ru.delivery.service.crud.CustomerCrudService;
+import ru.delivery.web.client.dto.YandexSuggestResponse;
+import ru.delivery.web.client.service.YandexSuggestService;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AddressService {
 
   private final CustomerCrudService customerCrudService;
   private final AddressMapper addressMapper;
+  private final YandexSuggestService yandexSuggestService;
 
   @Transactional
   public void addAddress(String userEmail, @Valid AddressDto addressDto) {
+    addressDto = validateAddressWithGeoSuggester(addressDto);
+
     var customer = customerCrudService.getByEmailWithAddresses(userEmail);
     customer.addAddress(addressMapper.addressDtoToAddress(addressDto, customer));
     customerCrudService.saveOrUpdate(customer);
+  }
+
+  /**
+   * Получаемый адрес отправляется в Yandex GeoSuggest, где ищутся соответствия ему.
+   * Если таковое находится, то его мы и заносим в БД
+   * @param addressDto
+   */
+  private AddressDto validateAddressWithGeoSuggester(@Valid AddressDto addressDto) {
+    var suggesterResponse = yandexSuggestService.suggest(addressDto.getCity() + " " + addressDto.getStreet() + " " + addressDto.getBuilding()).block();
+    if (suggesterResponse.getResults().isEmpty()) {
+      throw new BusinessLogicException("Некорректный адрес. GeoSuggest не смог найти соответствия");
+    }
+    var addressFromSuggester = suggesterResponse.getResults().get(0).getAddress();
+    log.info(addressFromSuggester.getFormatted_address());
+
+    var componentMap = YandexSuggestResponse.createComponentMap(addressFromSuggester);
+    return addressDto
+        .setCity(componentMap.get("LOCALITY"))
+        .setStreet(componentMap.get("STREET"))
+        .setBuilding(componentMap.get("HOUSE"));
   }
 
   @Transactional(readOnly = true)
---
+++ backend/src/main/java/ru/delivery/web/client/service/YandexSuggestService.java
@@ -0,0 +1,56 @@
+package ru.delivery.web.client.service;
+
+import io.netty.handler.logging.LogLevel;
+import java.util.Optional;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
+import ru.delivery.web.client.dto.YandexSuggestResponse;
+
+@Service
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "clients.yandex.suggester")
+@Data
+public class YandexSuggestService {
+
+  private final HttpClient httpClient = HttpClient.create()
+      .wiretap(this.getClass().getCanonicalName(), LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL);
+  private final ClientHttpConnector conn = new ReactorClientHttpConnector(httpClient);
+
+  private final WebClient webClient = WebClient.builder()
+      .baseUrl("https://suggest-maps.yandex.ru")
+      .clientConnector(conn)
+      .build();
+
+  private String path;
+  private String apikey;
+  private String lang;
+  private String results;
+  private String ll;
+  private String types;
+  private String print_address;
+
+  public Mono<YandexSuggestResponse> suggest(String text) {
+    return webClient.get()
+        .uri(uriBuilder -> uriBuilder
+            .path(path)
+            .queryParam("apikey", apikey)
+            .queryParam("text", text)
+            .queryParamIfPresent("lang", Optional.ofNullable(lang))
+            .queryParamIfPresent("results", Optional.ofNullable(results))
+            .queryParamIfPresent("ll", Optional.ofNullable(ll))
+            .queryParamIfPresent("types", Optional.ofNullable(types))
+            .queryParamIfPresent("print_address", Optional.ofNullable(print_address))
+            .build())
+        .retrieve()
+        .bodyToMono(YandexSuggestResponse.class);
+  }
+
+}
