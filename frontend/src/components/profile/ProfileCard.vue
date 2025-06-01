bb7ab0d4daaac532894119d89efa1eba058635ef
CandleStudent <rolltake@yandex.ru>
2025-06-01 15:25:20 +0300

    Унифицировать дизайн полей в ProfileCard.vue

--- frontend/src/components/profile/ProfileCard.vue
+++ frontend/src/components/profile/ProfileCard.vue
@@ -52,17 +52,17 @@
 
     <div>
       <label class="block text-sm mb-1 text-gray-600">Фамилия</label>
-      <input v-model="form.surname" class="input" placeholder="Введите фамилию" />
+      <input v-model="form.surname" class="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-500" placeholder="Введите фамилию" />
     </div>
 
     <div>
       <label class="block text-sm mb-1 text-gray-600">Телефон</label>
-      <input v-model="form.phone" class="input" placeholder="+7 (___) ___-__-__" />
+      <input v-model="form.phone" class="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-500" placeholder="+7 (___) ___-__-__" />
     </div>
 
     <div>
       <label class="block text-sm mb-1 text-gray-600">Электронная почта</label>
-      <input :value="form.email" class="input bg-gray-100 cursor-not-allowed" disabled />
+      <input :value="form.email" class="input bg-gray-100 cursor-not-allowed w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-500" disabled />
     </div>
 
     <button @click="updateProfile" class="w-full bg-green-600 hover:bg-green-700 text-white py-2 rounded transition">
