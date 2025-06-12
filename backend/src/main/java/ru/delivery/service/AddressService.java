582494312c10b31e55601cc1f8561ce650df3d6b
CandleStudent <rolltake@yandex.ru>
2025-06-13 02:42:25 +0300

    backend: удалить ненужный geosuggest на бэке

--- backend/src/main/java/ru/delivery/service/AddressService.java
+++ backend/src/main/java/ru/delivery/service/AddressService.java
@@ -12,9 +12,7 @@ import ru.delivery.exception.BusinessLogicException;
 import ru.delivery.mapper.AddressMapper;
 import ru.delivery.service.crud.AddressCrudService;
 import ru.delivery.service.crud.CustomerCrudService;
-import ru.delivery.web.client.dto.YandexSuggestResponse;
 import ru.delivery.web.client.service.YandexGeocoderService;
-import ru.delivery.web.client.service.YandexSuggestService;
 
 @Service
 @RequiredArgsConstructor
@@ -24,13 +22,11 @@ public class AddressService {
   private final CustomerCrudService customerCrudService;
   private final AddressCrudService addressCrudService;
   private final AddressMapper addressMapper;
-  private final YandexSuggestService yandexSuggestService;
   private final YandexGeocoderService yandexGeocoderService;
 
   @Transactional
   public void addAddress(String userEmail, @Valid AddressDto addressDto) {
     //todo split this method on more specific and SOLID
-    addressDto = validateAddressWithGeoSuggester(addressDto);
     var posOfAddress = yandexGeocoderService
         .getLonLatByAddress(addressDto.getFormattedAddress())
         .block();
@@ -43,29 +39,6 @@ public class AddressService {
 
     customer.addAddress(addressMapper.addressDtoToAddress(addressDto, customer));
     customerCrudService.saveOrUpdate(customer);
-  }
-
-  /**
-   * Получаемый адрес отправляется в Yandex GeoSuggest, где ищутся соответствия ему. Если таковое
-   * находится, то его мы и заносим в БД
-   *
-   * @param addressDto
-   */
-  private AddressDto validateAddressWithGeoSuggester(@Valid AddressDto addressDto) {
-    var suggesterResponse = yandexSuggestService.suggest(
-            addressDto.getCity() + " " + addressDto.getStreet() + " " + addressDto.getBuilding())
-        .block();
-    if (suggesterResponse.getResults().isEmpty()) {
-      throw new BusinessLogicException("Некорректный адрес. GeoSuggest не смог найти соответствия");
-    }
-    var addressFromSuggester = suggesterResponse.getResults().get(0).getAddress();
-    log.info(addressFromSuggester.getFormatted_address());
-
-    var componentMap = YandexSuggestResponse.createComponentMap(addressFromSuggester);
-    return addressDto
-        .setCity(componentMap.get("LOCALITY"))
-        .setStreet(componentMap.get("STREET"))
-        .setBuilding(componentMap.get("HOUSE"));
   }
 
   @Transactional(readOnly = true)
