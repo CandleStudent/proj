f22c943d4a25222a01ab01a0d1b4d6e3b4318f69
CandleStudent <rolltake@yandex.ru>
2025-06-13 14:03:17 +0300

    backend: убрать ResponseEntity как возвращаемое значение в методах контроллера OrderController

--- backend/src/main/java/ru/delivery/web/api/controller/OrderController.java
+++ backend/src/main/java/ru/delivery/web/api/controller/OrderController.java
@@ -2,8 +2,8 @@ package ru.delivery.web.api.controller;
 
 import jakarta.validation.Valid;
 import java.security.Principal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.delivery.dto.ActiveOrderDto;
 import ru.delivery.dto.NewOrderDto;
 import ru.delivery.dto.UpdatedOrderDto;
 import ru.delivery.service.OrderService;
@@ -24,31 +25,20 @@ public class OrderController {
   private final OrderService orderService;
 
   @GetMapping("/active")
-  public ResponseEntity<?> getActiveOrders(Principal principal) {
+  public List<ActiveOrderDto> getActiveOrders(Principal principal) {
 
     var userEmail = principal.getName();
 
-    try {
-      var activeOrders = orderService.getActiveOrders(userEmail);
-      return ResponseEntity.ok(activeOrders);
-    } catch (RuntimeException e) {
-      return ResponseEntity.badRequest().body(e.getMessage());
-    }
+    return orderService.getActiveOrders(userEmail);
   }
 
   @PostMapping("create")
-  public ResponseEntity<?> postOrder(
+  public void postOrder(
       Principal principal,
       @Valid @RequestBody NewOrderDto newOrderDto) {
 
     var userEmail = principal.getName();
-
-    try {
-      orderService.makeOrder(userEmail, newOrderDto);
-      return ResponseEntity.ok().build();
-    } catch (RuntimeException e) {
-      return ResponseEntity.badRequest().body(e.getMessage());
-    }
+    orderService.makeOrder(userEmail, newOrderDto);
   }
 
   @PutMapping("/update/{id}")
